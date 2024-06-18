50bfa53d25629e47f80d1eea007ff0afc7546076
Mishulka <misailgromov@gmail.com>
2024-06-18 20:34:18 +0300

    create logic to form. add controlled elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
@@ -22,6 +23,9 @@ function Logo() {
 
 function Form() {
 
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
   }
@@ -30,12 +34,17 @@ function Form() {
   return (
   <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for you trip?</h3>
-    <select>
+    <select value={quantity} onChange={e=>setQuantity(Number(e.target.value))}>
       {Array.from({length: 20}, ( num, i) => i + 1).map
       (num=><option value={num} key={num}>{num}</option>)
       }
     </select>
-    <input type='text' placeholder='Item...'/>
+    <input 
+    type='text' 
+    placeholder='Item...' 
+    value={description} 
+    onChange={(e) => setDescription(e.target.value)}
+    />
     <button>Add</button>
   </form>
   );
